d042c5b66b869079b924cc904ee48d0918e93f7b
Jose Morales <student.133716@worc.k12.ma.us>
2019-01-11 18:33:54 +0000

    Learn Python

--- ex45.py
+++ ex45.py
@@ -1,20 +1,22 @@
 from sys import exit
 from textwrap import dedent
 
-bullet = True
+bullets = 0
+bullet = False
+flashlight = False
 first_start = True
 first_gate = True
-hasBullets = True
-bullets = 6
 first_house = True
 first_woods = True
+first_open_field = True
 first_shack = True
 
 def start_room():
     global first_start
-    global bullets
-    global hasBullets
+    global bullet
+    global flashlight
     good = True
+    
     if first_start:
         print("What will you do?")
         first_start = False
@@ -23,16 +25,28 @@ def start_room():
     
     choice = input("> ")
     while good: 
-        if (choice == "pick up bullets" or choice == "take bullets") and bullet == True:
+        if (choice == "pick up bullets" or choice == "take bullets") and bullet == False:
             bullets = 6
-            hasBullets = False
+            bullet = True
+            
+            print("taken")
+            choice = input("> ")                    ## whenever take bullets and press enter it does the function but 
+                                                    ## when you type something after it wont register. So have to type again
+                                                    ## Its the same with the flashlight.
+            
+        elif (choice == "pick up bullets" or choice == "take bullets") and bullet == True:
+            print("You already have the bullets.")
+            
+            choice = input("> ")
+            
+        elif (choice == "pick up flashlight" or choice == "take flashlight") and flashlight == False:
+            flashlight = True
             
             print("taken")
             choice = input("> ")
-            
-        elif(choice == "pick up bullets" or choice == "take bullets") and bullet == False:
-            print("You already have the bullets.")
-            
+        
+        elif (choice == "pick up flashlight" or choice == "take flashlight") and flashlight == True:
+            print("You already have the flashlight.")
             choice = input("> ")
             
         elif choice == "go north" or choice == "walk north":
@@ -80,6 +94,17 @@ def gate():
         choice = input("> ")
         if choice == "go south" or choice == "walk south":
             start_room()
+        elif choice == "look around":
+            print(dedent("""
+            The big wall is still there and you need
+            the keycard to get through the door. 
+            Good luck with that. The only way you 
+            could go is south.
+            """))
+            
+        elif (choice == "go north") or (choice == "go west") or (choice == "go east"):
+            print("Can't go that way dummy.")
+            
         else:
             print("I dont know what that means.")
             choice = input("> ")
@@ -107,6 +132,17 @@ def house():
         
         if choice == "go north" or choice == "walk north":
             start_room()
+            
+        elif choice == "look around":
+            print(dedent("""
+            The house has a cracked window which seems 
+            like you could break. *COUGH* *COUGH*
+            The only path is to the north.
+            """))
+        
+        elif (choice == "go south") or (choice == "go east") or (choice == "go west"):
+            print("You can't go that way dum dum.")
+            
         else:
             print("I don't know what that means")
     
@@ -132,10 +168,52 @@ def woods():
         
         if choice == "go east" or choice == "walk east":
             start_room()
+            
+        elif choice == "go south" or choice == "walk south":
+            open_field()
+            
+        elif choice == "look around":
+            print(dedent("""
+            You're currently deep in the woods and you
+            have a feeling you're being watched. The
+            only paths are south and east to where
+            you came.
+            """))
+            
+        elif (choice == "go north") or (choice == "go west"):
+            print("Wrong way dum dum")
+            
         else:
             print("I dont know what that means.")
             choice = input("> ")
     
+
+def open_field():
+    global first_open_field
+    good = True
+    
+    if first_open_field:
+        print(dedent("""
+        You walk south of the woods and arrive at an open
+        field of grass. A few miles away is the wall 
+        surrounding the entire area you're in. There 
+        seems to be nothing around but grass around you.
+        The only path is back north.
+        """))
+        
+        first_open_field = False
+    else:
+        print("Open Field")
+    
+    choice = input("> ")
+    
+    while good:
+        if choice == "go north" or choice == "walk north":
+            woods()
+        else:
+            print("I dont know what that means.")
+            choice = input("> ")
+        
     
     
 
@@ -160,6 +238,17 @@ def shack():
         
         if choice == "go west" or choice == "walk west":
             start_room()
+            
+        elif choice == "look around":
+            print(dedent("""
+            The shack has no door. Maybe you should
+            go in. Dumb dumb. The only path is to 
+            the west.
+            """))
+            
+        elif (choice == "go north") or (choice == "go east") or (choice == "go south"):
+            print("No not that way silly.")
+            
         else:
             print("I dont know what that means.")
             choice = input("> ")
